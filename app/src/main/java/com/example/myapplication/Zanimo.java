c37cd7d381e53bacb88912981ce307e066ffa0ca
Zanimonetc Mikhail <z_a_r_k@mail.ru>
2022-11-02 20:22:36 +0300

    commit new hello!

--- app/src/main/java/com/example/myapplication/Zanimo.java
+++ app/src/main/java/com/example/myapplication/Zanimo.java
@@ -3,5 +3,6 @@ package com.example.myapplication;
 public class Zanimo {
     public static void main(String[] args){
         System.out.println ("Hello, World!");
+        System.out.println ("Hello!");
     }
 }
