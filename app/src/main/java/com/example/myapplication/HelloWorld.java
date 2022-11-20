75ea9c4bce78937ba526ef0927dc497c8d26e84d
Zanimonetc Mikhail <z_a_r_k@mail.ru>
2022-11-20 13:38:13 +0300

    00

--- app/src/main/java/com/example/myapplication/HelloWorld.java
+++ app/src/main/java/com/example/myapplication/HelloWorld.java
@@ -1,7 +1,27 @@
 package com.example.myapplication;
 
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main (String[] args){
-        System.out.println ("Hello, Master!");
+       Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+       int m = in.nextInt();
+
+       printInConsole(m);
+
+    }
+
+    static void printInConsole(int m){
+        switch (m){
+            case 1 :
+                System.out.println("это 1");
+                break;
+            case 2 :
+                System.out.println("это 2");
+                break;
+            default:
+                System.out.println("я не знаю это число");
+        }
     }
 }
